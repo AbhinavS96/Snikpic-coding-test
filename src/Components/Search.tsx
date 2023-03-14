42b6476d7167ee2c3a235c5ee106172866ed1737
Abhinav <abhinavsidharth@gmail.com>
2023-03-14 14:25:32 +0100

    got data from api

--- src/Components/Search.tsx
+++ src/Components/Search.tsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
 
+const key = "d37dede0";
+const URL = `http://www.omdbapi.com/?apikey=${key}&`;
+
 const Search: React.FC = () => {
   const [input, setInput] = useState("");
   const inputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInput(event.target.value);
   };
+  const doSearch = async () => {
+    const response = await fetch(`${URL}s=${input}`);
+    const result = await response.json();
+    console.log(result);
+  };
   const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    doSearch();
+    setInput("");
   };
   return (
     <>
