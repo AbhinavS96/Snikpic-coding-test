489126526f5b429c09debb7ead04aadb4dc6cb48
Abhinav <abhinavsidharth@gmail.com>
2023-03-14 15:27:06 +0100

    did basic css

--- src/Components/MovieList.tsx
+++ src/Components/MovieList.tsx
@@ -1,12 +1,13 @@
-import { movieType } from "../Models/Movie";
+import { movieType } from "../Models/movie";
 import Movie from "./Movie";
+import styles from "./MovieList.module.css";
 const MovieList: React.FC<{ movies: movieType[] | [] }> = (props) => {
   return (
     <>
       <ul>
         {props.movies.map((movie) => {
           return (
-            <li key={movie.imdbID}>
+            <li key={movie.imdbID} className={styles.movieList}>
               <Movie movie={movie}></Movie>
             </li>
           );
